8dbd280656964c0fb6a6e6a2a7184d679d1ecf0b
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-04-18 20:33:14 +1000

    Added picture model

---
+++ app/portfolio/models/picture.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.db.models import ImageField, CharField, DateField, ManyToManyField
+from django.utils.text import slugify
+
+
+class Picture(models.Model):
+    picture_image = ImageField(upload_to='picture_image')
+    title = CharField(max_length=69)
+    slug = CharField(max_length=69)
+    location = CharField(max_length=169)
+    taken_date = DateField()
+    subjects = ManyToManyField('Subject')
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Picture, self).save(*args, **kwargs)
